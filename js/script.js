9328498271d79421c17c0ba1d0b1301829810075
ProfessorSebastiaoMarcos <profsebastiaomarcos@gmail.com>
2025-06-27 20:05:57 -0300

    maior e menor

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-document.getElementById('botao-exa').addEventListener('click', function () {
+document.getElementById('botao-par-impar').addEventListener('click', function () {
     parImpar()
 });
+
+document.getElementById('botao-maior-menor').addEventListener('click', function () {
+    maiorMenor()
+});
+
+
 
 function parImpar() {
         const entrada = prompt('Digite um número: ');
@@ -10,13 +16,16 @@ function parImpar() {
             return;
         }
 
+        // Casting
         const numero = Number(entrada);
 
+        // Validação
         if (isNaN(numero)) {
             alert('Não é um número!');
             return;
         }
 
+        //Testando
         if (numero % 2 === 0) {
             alert(`O número ${numero} é par!`)
         } else {
